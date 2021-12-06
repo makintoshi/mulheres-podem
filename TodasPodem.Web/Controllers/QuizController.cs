2b11217dffdd411cbb7fa17897d15366f65374de
makintoshi <amanda.makino@gmail.com>
2021-12-05 22:05:32 -0200

    Added site template

---
+++ TodasPodem.Web/Controllers/QuizController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodasPodem.Web.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TodasPodem.Web/Models/QuizViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodasPodem.Web.Models
+{
+    public class QuizViewModel
+    {
+        public string Name { get; set; }
+
+        public string IdentityNumber { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string History { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Address { get; set; }
+
+        public string Number { get; set; }
+
+        public string Complement { get; set; }
+
+        public string District { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+    }
+}
